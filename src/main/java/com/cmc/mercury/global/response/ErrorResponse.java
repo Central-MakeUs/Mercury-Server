bf645180c617d86711ea3f7e731925824c40a1e5
dainnida <dain0928@g.hongik.ac.kr>
2025-01-17 21:21:15 +0900

    [#4] Feat: 공통 Response들과 swagger 연결

--- src/main/java/com/cmc/mercury/global/response/ErrorResponse.java
+++ src/main/java/com/cmc/mercury/global/response/ErrorResponse.java
@@ -1,14 +1,18 @@
 package com.cmc.mercury.global.response;
 
 import com.cmc.mercury.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 //@RequiredArgsConstructor
+@Schema(description = "공통 에러 응답 형식")
 public class ErrorResponse {
 
+    @Schema(description = "에러 코드", example = "Common400")
     private final String code;
+    @Schema(description = "에러 메시지", example = "적절하지 않은 요청입니다.")
     private final String message;
 
     public ErrorResponse (ErrorCode errorCode) {
