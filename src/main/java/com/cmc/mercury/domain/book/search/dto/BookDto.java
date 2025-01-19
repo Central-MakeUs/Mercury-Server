5b9b7c5e766f09a0828c51014e5931315420061f
dainnida <dain0928@g.hongik.ac.kr>
2025-01-19 13:08:18 +0900

    [#9] Docs: 도서 검색 관련 Dto들 swagger 문서화

--- src/main/java/com/cmc/mercury/domain/book/search/dto/BookDto.java
+++ src/main/java/com/cmc/mercury/domain/book/search/dto/BookDto.java
@@ -1,13 +1,20 @@
 package com.cmc.mercury.domain.book.search.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.json.JSONObject;
 
+@Schema(description = "알라딘 API 응답의 도서 부분 파싱 형식")
 public record BookDto(
 
+        @Schema(description = "제목")
         String title,
+        @Schema(description = "표지 이미지")
         String coverImageUrl,
+        @Schema(description = "저자")
         String author,
+        @Schema(description = "isbn 13자리")
         String isbn13,
+        @Schema(description = "구매 링크")
         String link
 ) {
     // json(item 부분만) -> Book
