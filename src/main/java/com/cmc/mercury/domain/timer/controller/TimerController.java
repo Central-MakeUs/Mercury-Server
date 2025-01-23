74ba25b571f162bde798caec92855abfc1d7e27f
dainnida <dain0928@g.hongik.ac.kr>
2025-01-24 00:50:54 +0900

    [#40] Feat: TimerController 작성

---
+++ src/main/java/com/cmc/mercury/domain/timer/controller/TimerController.java
@@ -0,0 +1,42 @@
+package com.cmc.mercury.domain.timer.controller;
+
+import com.cmc.mercury.domain.timer.dto.TimerListResponse;
+import com.cmc.mercury.domain.timer.dto.TimerRequest;
+import com.cmc.mercury.domain.timer.dto.TimerResponse;
+import com.cmc.mercury.domain.timer.service.TimerService;
+import com.cmc.mercury.global.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/timers")
+@RequiredArgsConstructor
+@Tag(name = "TimerController", description = "타이머 관련 API")
+public class TimerController {
+
+    private final TimerService timerService;
+
+    @PostMapping
+    @Operation(summary = "타이머 기록 등록", description = "타이머 기록을 생성합니다.")
+    public SuccessResponse<TimerResponse> createTimer(
+            @RequestParam("userId") Long testUserId,
+            @RequestBody @Valid TimerRequest request
+    ) {
+        return SuccessResponse.created(
+                timerService.createTimer(testUserId, request)
+        );
+    }
+
+    @GetMapping
+    @Operation(summary = "타이머 기록 목록 조회", description = "타이머 기록 리스트를 반환합니다.")
+    public SuccessResponse<TimerListResponse> getTimers(
+            @RequestParam("userId") Long testUserId
+    ) {
+        return SuccessResponse.ok(
+                timerService.getTimerList(testUserId)
+        );
+    }
+}
