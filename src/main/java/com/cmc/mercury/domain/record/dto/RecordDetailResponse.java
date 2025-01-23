0b28b975c526ab32705f270775210749d4734eec
dainnida <dain0928@g.hongik.ac.kr>
2025-01-23 12:15:19 +0900

    [#22] Feature: Record와 RecordDetail 응답 형식 생성

---
+++ src/main/java/com/cmc/mercury/domain/record/dto/RecordDetailResponse.java
@@ -0,0 +1,38 @@
+package com.cmc.mercury.domain.record.dto;
+
+import com.cmc.mercury.domain.book.dto.BookResponse;
+import com.cmc.mercury.domain.memo.dto.MemoResponse;
+import com.cmc.mercury.domain.record.entity.Record;
+import com.cmc.mercury.domain.record.entity.RecordDetail;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Schema(title = "기록 상세 조회 응답 형식")
+public record RecordDetailResponse(
+
+        @Schema(description = "기록 객체 ID")
+        Long recordId,
+        @Schema(description = "최근 독서 진도율")
+        int updatedGauge,
+        @Schema(description = "도서 정보")
+        BookResponse book,
+        @Schema(description = "생성 일시")
+        LocalDateTime createdAt,
+        @Schema(description = "수정 일시")
+        LocalDateTime updatedAt,
+        @Schema(description = "메모 객체 목록")
+        List<MemoResponse> memos
+) {
+        public static RecordDetailResponse of(Record record, RecordDetail detail, List<MemoResponse> memos) {
+                return new RecordDetailResponse(
+                        record.getId(),
+                        detail.getUpdatedGauge(),
+                        BookResponse.from(record.getBook()),
+                        record.getCreatedAt(),
+                        record.getUpdatedAt(),
+                        memos
+                );
+        }
+}
---
+++ src/main/java/com/cmc/mercury/domain/record/dto/RecordResponse.java
@@ -0,0 +1,37 @@
+package com.cmc.mercury.domain.record.dto;
+
+import com.cmc.mercury.domain.book.dto.BookResponse;
+import com.cmc.mercury.domain.record.entity.Record;
+import com.cmc.mercury.domain.record.entity.RecordDetail;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+@Schema(title = "기록 객체 응답 형식")
+public record RecordResponse (
+
+        @Schema(description = "기록 객체 ID")
+        Long recordId,
+        @Schema(description = "최근 독서 진도율")
+        int updatedGauge,
+        @Schema(description = "최근 메모 내용")
+        String latestMemoContent,
+        @Schema(description = "생성 일시")
+        LocalDateTime createdAt,
+        @Schema(description = "수정 일시")
+        LocalDateTime updatedAt,
+        @Schema(description = "도서 정보")
+        BookResponse book
+) {
+        public static RecordResponse of(Record record, RecordDetail detail, String content) {
+
+                return new RecordResponse(
+                        record.getId(),
+                        detail.getUpdatedGauge(),
+                        content,
+                        record.getCreatedAt(),
+                        record.getUpdatedAt(),
+                        BookResponse.from(record.getBook())
+                );
+        }
+}
