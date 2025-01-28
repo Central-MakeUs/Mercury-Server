1891ef21e01b0ebe4da53a0f896633c70998823e
dainnida <dain0928@g.hongik.ac.kr>
2025-01-28 23:47:29 +0900

    [#46] Feat: 선택 도서 중복 검사 로직 구현 및 api 연결

--- src/main/java/com/cmc/mercury/domain/book/controller/BookController.java
+++ src/main/java/com/cmc/mercury/domain/book/controller/BookController.java
@@ -3,37 +3,38 @@ package com.cmc.mercury.domain.book.controller;
 import com.cmc.mercury.domain.book.dto.BookExistResponse;
 import com.cmc.mercury.domain.book.dto.BookSearchRequest;
 import com.cmc.mercury.domain.book.dto.BookSearchResponse;
-import com.cmc.mercury.domain.book.service.BookSearchService;
+import com.cmc.mercury.domain.book.service.BookService;
 import com.cmc.mercury.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
-@Tag(name = "BookSearchController", description = "도서 검색 관련 API")
-public class BookSearchController {
+@Tag(name = "BookController", description = "도서 검색 관련 API")
+public class BookController {
 
-    private final BookSearchService bookSearchService;
+    private final BookService bookService;
 
     @GetMapping("/search")
     @Operation(summary = "도서 검색", description = "검색어를 통해 도서 목록을 정렬하여 반환합니다.")
     public Mono<SuccessResponse<BookSearchResponse>> searchBooks(
             @ModelAttribute @Valid BookSearchRequest request) {
-        return bookSearchService.searchBooks(request)
+        return bookService.searchBooks(request)
                 .map(SuccessResponse::ok);
     }
 
     @GetMapping("/exist")
     @Operation(summary = "기록 중복 생성 검사",
             description = "isbn을 통해 해당 도서에 대한 기록을 생성한 적이 있는지의 여부를 반환합니다.")
-    public SuccessResponse<BookExistResponse> 
-
+    public SuccessResponse<BookExistResponse> existBooks(
+            @RequestParam("userId") Long testUserId,
+            @RequestParam String isbn13
+    ) {
+        return SuccessResponse.ok(bookService.existBooks(testUserId, isbn13));
+    }
 }
