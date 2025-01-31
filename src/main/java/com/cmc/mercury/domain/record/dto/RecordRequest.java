fcf2b8ca1e2328fe5cf426a0cd69fe0fa4d5a58f
dainnida <dain0928@g.hongik.ac.kr>
2025-01-31 12:40:52 +0900

    [#49] Feat: 신규 기록 생성과 메모 추가 시 request에 단말 시간 필드 추가

--- src/main/java/com/cmc/mercury/domain/record/dto/RecordRequest.java
+++ src/main/java/com/cmc/mercury/domain/record/dto/RecordRequest.java
@@ -4,6 +4,8 @@ import com.cmc.mercury.domain.book.dto.BookDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDateTime;
+
 @Schema(title = "신규 기록 객체 생성 요청 형식")
 public record RecordRequest (
 
@@ -16,7 +18,12 @@ public record RecordRequest (
 
 //        @NotNull(message = "진도율은 필수입니다.")
         @Schema(description = "독서 진도율")
-        int gauge
+        int gauge,
+
+        @NotNull(message = "단말 시간은 필수입니다.")
+        @Schema(description = "사용자 단말 시간 (ISO-8601 형식)",
+        example = "2024-01-30T12:34:56")
+        LocalDateTime deviceTime
 ){
 
 }
