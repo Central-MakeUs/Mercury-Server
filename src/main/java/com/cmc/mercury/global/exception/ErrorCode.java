3c91151222bd5dbea4edb0dbcc4e6ce6efa3f009
dainnida <dain0928@g.hongik.ac.kr>
2025-02-05 21:48:41 +0900

    [#58] Feat: oauth와 jwt에서의 error code 추가

--- src/main/java/com/cmc/mercury/global/exception/ErrorCode.java
+++ src/main/java/com/cmc/mercury/global/exception/ErrorCode.java
@@ -44,9 +44,19 @@ public enum ErrorCode {
 
     // Memo
     MEMO_NOT_BELONG_TO_RECORD(HttpStatus.BAD_REQUEST, "Memo400", "해당 독서 기록 객체에 속한 메모가 아닙니다."),
-    MEMO_NOT_FOUND(HttpStatus.NOT_FOUND, "Memo404", "메모를 찾을 수 없습니다.");
+    MEMO_NOT_FOUND(HttpStatus.NOT_FOUND, "Memo404", "메모를 찾을 수 없습니다."),
     // MEMO_CREATE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Memo500", "메모 생성에 실패했습니다.");
 
+    // OAuth
+    INVALID_OAUTH2_PROVIDER(HttpStatus.BAD_REQUEST, "OAuth400", "지원하지 않는 소셜 로그인 제공자입니다."),
+    OAUTH2_PROCESSING_ERROR(HttpStatus.UNAUTHORIZED, "OAuth401", "소셜 로그인 처리 중 오류가 발생했습니다."),
+    OAUTH2_LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "OAuth401", "소셜 로그인에 실패했습니다."),
+
+    // JWT
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Jwt401", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "Jwt401", "만료된 토큰입니다."),
+    TOKEN_TYPE_MISMATCH(HttpStatus.UNAUTHORIZED, "Jwt401", "토큰 타입이 일치하지 않습니다."),
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "Jwt401", "토큰이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
