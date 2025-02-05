76f580c73412cf577ab702c9d5c0ca3acfc4c1f7
dainnida <dain0928@g.hongik.ac.kr>
2025-02-05 20:57:28 +0900

    [#58] Feat: 소셜로그인 반영하여 User 엔티티 수정

---
+++ src/main/java/com/cmc/mercury/domain/user/entity/OAuthType.java
@@ -0,0 +1,5 @@
+package com.cmc.mercury.domain.user.entity;
+
+public enum OAuthType {
+    APPLE, GOOGLE, KAKAO
+}
---
+++ src/main/java/com/cmc/mercury/domain/user/entity/UserStatus.java
@@ -0,0 +1,5 @@
+package com.cmc.mercury.domain.user.entity;
+
+public enum UserStatus {
+    ACTIVE, INACTIVE
+}
