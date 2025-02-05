be4ebecbebfda9bb59206088d0d2108dd17b824b
dainnida <dain0928@g.hongik.ac.kr>
2025-02-05 23:16:43 +0900

    [#58] Feat: 인증 관련 config 작성

--- src/main/java/com/cmc/mercury/global/config/SwaggerConfig.java
+++ src/main/java/com/cmc/mercury/global/config/SwaggerConfig.java
@@ -3,11 +3,14 @@ package com.cmc.mercury.global.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -21,12 +24,32 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        // SecuritySchemes 등록
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .name("JwtAuthorization")
+                .scheme("Bearer")
+                .bearerFormat("JWT");
+
+        SecurityScheme cookieAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name("refresh_token");
+
         return new OpenAPI()
                 .servers(List.of(
                         new Server().url(httpsUrl).description("배포(HTTPS)"),
-                        new Server().url(httpUrl).description("로컬(HTTP)")
+                        new Server().url(httpUrl).description("로컬(HTTP)"),
+                        new Server().url("http://localhost:8080").description("localhost")
                 ))
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", bearerAuth)
+                        .addSecuritySchemes("cookie-refresh", cookieAuth))
+                .security(Arrays.asList(
+                        new SecurityRequirement().addList("bearer-jwt"),
+                        new SecurityRequirement().addList("cookie-refresh")))
                 .info(apiInfo());
     }
     private Info apiInfo() {
