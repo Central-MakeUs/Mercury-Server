a02e048d627e8e6a1e0470a4188947fbf4ce81d1
dainnida <dain0928@g.hongik.ac.kr>
2025-02-12 02:32:06 +0900

    Refactor: 소셜로그인 성공 시 회원가입인지 구분 및 실패 시 에러 메시지 포함되도록 수정

--- src/main/java/com/cmc/mercury/global/oauth/handler/OAuth2FailureHandler.java
+++ src/main/java/com/cmc/mercury/global/oauth/handler/OAuth2FailureHandler.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @Slf4j
@@ -19,8 +21,11 @@ public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler 
                                         AuthenticationException exception) throws IOException {
 
         log.error("Social Login Failed: {}", exception.getMessage());
+        // 예외 메시지를 URI로 인코딩하여 전달
+        String errorMessage = URLEncoder.encode(exception.getMessage(), StandardCharsets.UTF_8);
 
         String targetUrl = UriComponentsBuilder.fromUriString("https://www.mercuryplanet.co.kr/login/fail")
+                .queryParam("error", errorMessage)
                 .build(true).toUriString();
 
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
